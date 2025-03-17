db47e00a2b922eec5b115774a97d695e51516690
BSCDulak <110743178+BSCDulak@users.noreply.github.com>
2025-03-17 14:49:07 +0100

    add content
    add TourLog.cs
    copy ButtonControl in MainWindowVieModel to prepare for tourlogs
    add base grid for tab
    add attribute to TourDetailsWrapPanel.xaml

---
+++ TourPlanner/Models/TourLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWEN2_TourPlannerGroupProject.Models
+{
+    class TourLog
+    {
+        public string? Date { get; set; }
+        public string? TotalTime { get; set; }
+        public string? Report { get; set; }
+        public string? Distance { get; set; }
+        public string? Rating { get; set; }
+        public string? AverageSpeed { get; set; }
+
+    }
+}
