c91f7e0439b8ed71153738c69faeba619ee338cb
jimemogo20 <jimemogo@gmail.com>
2025-03-18 16:14:03 +0100

    changes

--- TourPlanner/Models/TourLog.cs
+++ TourPlanner/Models/TourLog.cs
@@ -14,6 +14,12 @@ namespace SWEN2_TourPlannerGroupProject.Models
         public string? Distance { get; set; }
         public string? Rating { get; set; }
         public string? AverageSpeed { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Comment { get; set; }
+        public required string Difficulty { get; set; }
+        public double TotalDistance { get; set; }
+        public TimeSpan TimeSpan { get; set; }
+        
 
     }
 }
