5bc3e59806211ec98d9b6646b6bf3a58fa3fb215
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-07-02 17:20:38 +0200

    create appsettings.json and change App.xaml.cs so that the connection string can be used everywhere.

--- TourPlanner/App.xaml.cs
+++ TourPlanner/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
 
 namespace SWEN2_TourPlannerGroupProject;
@@ -9,5 +11,19 @@ namespace SWEN2_TourPlannerGroupProject;
 /// </summary>
 public partial class App : Application
 {
-}
+    public static string ConnectionString { get; private set; }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        ConnectionString = config.GetConnectionString("DefaultConnection");
+
+        // Now you can use App.ConnectionString anywhere in your app
+    }
+}
