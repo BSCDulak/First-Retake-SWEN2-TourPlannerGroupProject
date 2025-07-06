635b06b885db9f0dc65404369a2bab5e0ec35d1d
jimemogo20 <jimemogo@gmail.com>
2025-07-05 23:05:49 -0500

    What´s done till now
    ✔ Uses C# or Java
    ✔ Uses markup-based UI framework (WPF/XAML)
    ✔ Uses MVVM for UI
    ✔ Layered architecture (UI / BL / DAL)
    ✔ At least one design pattern (RelayCommand, MVVM itself)
    ✔ Uses Leaflet and OpenRouteService (demostrado con el mapa)
    ✔ Uses config file (appsettings.json)
    ✔ Uses OR-Mapping Library (EF Core)

---
+++ TourPlanner/Views/Subviews/OpenRouteServiceMap.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.IO;
+using System.Windows.Controls;
+
+namespace SWEN2_TourPlannerGroupProject.Views.Subviews
+{
+    public partial class OpenRouteServiceMap : UserControl
+    {
+        public OpenRouteServiceMap()
+        {
+            InitializeComponent();
+            LoadMap();
+        }
+
+        private void LoadMap()
+        {
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MapHtml", "map.html");
+            MapView.Source = new Uri(path);
+        }
+
+    }
+}
+
+
