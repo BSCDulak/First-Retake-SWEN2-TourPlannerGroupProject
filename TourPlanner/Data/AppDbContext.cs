e7f94c60f6c25e50094366864ed95b34fbcfdbbf
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-08-11 16:30:28 +0200

    Create AppDBContext and AppDbContextFactory (for design time use)

---
+++ TourPlanner/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWEN2_TourPlannerGroupProject.Models;
+
+namespace SWEN2_TourPlannerGroupProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tour> Tours { get; set; }
+        public DbSet<TourLog> TourLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tour>()
+                .HasMany(t => t.TourLogs)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
