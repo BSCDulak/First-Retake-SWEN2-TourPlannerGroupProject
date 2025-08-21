4a377ba04007df97b7b0448747b3e327375f5ef8
sherlock0062 <bettinakovac032@gmail.com>
2025-08-21 23:07:45 +0200

    Nothing crashes safe point

--- TourPlanner/ViewModels/ToursListViewModel.cs
+++ TourPlanner/ViewModels/ToursListViewModel.cs
@@ -6,6 +6,9 @@ using SWEN2_TourPlannerGroupProject.Data;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
+using System.IO; // ✅ Added for file output
+using iText.Kernel.Pdf; // ✅ iText
+using System.Windows; // ✅ For MessageBox
 
 namespace SWEN2_TourPlannerGroupProject.ViewModels
 {
@@ -34,7 +37,7 @@ namespace SWEN2_TourPlannerGroupProject.ViewModels
         public ICommand DeleteCommand { get; }
         public ICommand UpdateCommand { get; }
         public ICommand UpdateCalculationsCommand { get; }
-
+        public ICommand ReportCommand { get; } 
         public ToursListViewModel()
         {
             _instanceCounter++;
@@ -45,6 +48,7 @@ namespace SWEN2_TourPlannerGroupProject.ViewModels
             AddCommand = new RelayCommand(async _ => await AddTourAsync());
             DeleteCommand = new RelayCommand(async _ => await DeleteTourAsync(), _ => SelectedTour != null);
             UpdateCommand = new RelayCommand(async _ => await UpdateTourAsync(), _ => SelectedTour != null);
+            ReportCommand = new RelayCommand(_ => { /* Mock report action */ });
             UpdateCalculationsCommand = new RelayCommand(_ => UpdateAllCalculations());
             
             System.Diagnostics.Debug.WriteLine($"ToursListViewModel created. Tours count: {Tours.Count}");
