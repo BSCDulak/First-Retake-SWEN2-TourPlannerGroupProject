f4c10c540faf2cea61a71d12874cc09a72f030ad
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-08-21 11:35:29 +0200

    Update TourLogsViewModel.cs
    use the new log instead of System.Diagnostics and add some more log.Info lines

--- TourPlanner/ViewModels/TourLogsViewModel.cs
+++ TourPlanner/ViewModels/TourLogsViewModel.cs
@@ -9,12 +9,14 @@ using System.Collections.ObjectModel;
 using SWEN2_TourPlannerGroupProject.Data;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace SWEN2_TourPlannerGroupProject.ViewModels
 {
     internal class TourLogsViewModel : ViewModelBase
     {
         private readonly ToursListViewModel _toursListViewModel;
         private readonly ITourLogRepository _tourLogRepository;
+        private static readonly ILoggerWrapper log = LoggerFactory.GetLogger();
 
         public ObservableCollection<TourLog> TourLogs
         {
@@ -44,6 +46,7 @@ namespace SWEN2_TourPlannerGroupProject.ViewModels
             _toursListViewModel = toursListViewModel;
             _tourLogRepository = App.ServiceProvider.GetRequiredService<ITourLogRepository>();
             
+            log.Info("TourLogsViewModel constructor called.");
             _toursListViewModel.PropertyChanged += (sender, e) =>
             {
                 if (e.PropertyName == nameof(_toursListViewModel.SelectedTour))
@@ -73,6 +76,11 @@ namespace SWEN2_TourPlannerGroupProject.ViewModels
                 
                 var addedLog = await _tourLogRepository.AddTourLogAsync(newLog);
                 OnPropertyChanged(nameof(TourLogs));
+                log.Info("Adding new TourLog.");
+            }
+            else
+            {
+                log.Warn("No Tour selected to add a TourLog.");
             }
         }
 
@@ -80,21 +88,31 @@ namespace SWEN2_TourPlannerGroupProject.ViewModels
         {
             if (_toursListViewModel.SelectedTour != null && SelectedTourLog != null)
             {
+                log.Info($"Deleting TourLog: ID:{SelectedTourLog.TourLogId} Name:{SelectedTourLog.Name} of Tour ID:{SelectedTourLog.TourId} Name:{SelectedTourLog.Tour?.Name}");
                 if (SelectedTourLog.TourLogId.HasValue)
                 {
                     await _tourLogRepository.DeleteTourLogAsync(SelectedTourLog.TourLogId.Value);
                 }
+                else
+                {
+                    log.Warn("SelectedTourLog does not have a valid TourLogId.");
+                }
                 SelectedTourLog = null;
                 OnPropertyChanged(nameof(TourLogs));
+                log.Info("TourLog deleted successfully.");
             }
         }
 
         private async Task UpdateTourLogAsync()
         {
-            System.Diagnostics.Debug.WriteLine($"Updating TourLog: {SelectedTourLog?.TourLogId}");
+            log.Info($"Updating TourLog: ID:{SelectedTourLog?.TourLogId} Name:{SelectedTourLog?.Name} of Tour ID:{SelectedTourLog?.TourId} Name:{SelectedTourLog?.Tour?.Name}");
             if (SelectedTourLog != null && SelectedTourLog.TourLogId.HasValue)
             {
                 await _tourLogRepository.UpdateTourLogAsync(SelectedTourLog);
+            }
+            else
+            {
+                log.Warn("SelectedTourLog is null or does not have a valid TourLogId.");
             }
         }
 
