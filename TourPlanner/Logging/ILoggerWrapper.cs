0cd1f85e97a3532885199087cf266c29edee6aea
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-08-21 10:39:40 +0200

    Create Logging Folder with Logging Implementation and inject it into WPF project
    Create ILoggerWrapper.cs:
    This is the Interface we base our Log4NetWrapper.cs on so we don´t forget anything important.
    Create Log4NetWrapper.cs:
    Here we implement the functions defined in the ILoggerWrapper.cs
    Create LoggerFactory.cs:
    Here we create a class to read out our log4net.config so we have the configs we need.
    Change App.xaml.cs:
    We inject the LoggerFactory.GetLogger() function into our WPF.
    Change SWEN2-TourPlannerGroupProject.csproj:
    We also need to copy the log4net.config to the output directory or else it won´t be found by the LoggerFactory.

---
+++ TourPlanner/Logging/ILoggerWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWEN2_TourPlannerGroupProject.Logging
+{
+    public interface ILoggerWrapper
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Error(string message);
+        void Fatal(string message);
+        void Warn(string message);
+    }
+}
+
---
+++ TourPlanner/Logging/Log4NetWrapper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWEN2_TourPlannerGroupProject.Logging
+{
+    public class Log4NetWrapper : ILoggerWrapper
+    {
+        private readonly log4net.ILog logger;
+
+        public static Log4NetWrapper CreateLogger(string configPath, string caller)
+        {
+            if (!File.Exists(configPath))
+            {
+                throw new ArgumentException("Does not exist.", nameof(configPath));
+            }
+
+            log4net.Config.XmlConfigurator.Configure(new FileInfo(configPath));
+            var logger = log4net.LogManager.GetLogger(caller);  // System.Reflection.MethodBase.GetCurrentMethod().DeclaringType
+            return new Log4NetWrapper(logger);
+        }
+
+        private Log4NetWrapper(log4net.ILog logger)
+        {
+            this.logger = logger;
+        }
+
+        public void Debug(string message)
+        {
+            this.logger.Debug(message);
+        }
+        public void Info(string message)
+        {
+            this.logger.Info(message);
+        }
+        public void Warn(string message)
+        {
+            this.logger.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            this.logger.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            this.logger.Fatal(message);
+        }
+    }
+}
